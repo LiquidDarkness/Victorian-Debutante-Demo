a3a5787117f852227cc1fc66bc4f90da23c5be4e
LiquidDarkness <jumackholio@gmail.com>
2022-02-03 21:26:34 +0100

    shit's working added method SaveEndingData added loading existing PlayerPrefs on Awake

--- Assets/Scripts/StoryPivot.cs
+++ Assets/Scripts/StoryPivot.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,7 +6,24 @@ using UnityEngine;
 public class StoryPivot : Story
 {
     public List<Story> stories;
-    public List<int> endingPoints;
+    private List<int> endingPoints;
+    private const string savingKey = "savingKey serves as key in PlayerPrefs.SetString alongside savingData";
+
+    public void Awake()
+    {
+        if (PlayerPrefs.HasKey(savingKey))
+        {
+            string savingData = PlayerPrefs.GetString(savingKey);
+            endingPoints = ConvertData(savingData);
+            return;
+        }
+        endingPoints = new List<int>(stories.Count);
+    }
+
+    private List<int> ConvertData(string savingData)
+    {
+        // TODO: odtegowaæ do listy
+    }
 
     public Story GetResultingStory()
     {
@@ -34,8 +52,17 @@ public class StoryPivot : Story
             if (story == endingStory)
             {
                 endingPoints[i] += value;
-                return;
+                SaveEndingData();
+                return; // TODO: wykombinowaæ, po co tu jest ten return i co siê mo¿e staæ bez niego
             }
         }
+    }
+
+    private void SaveEndingData()
+    {
+        // example:
+        // 32,12,9,15
+        string savingData = string.Join(",", endingPoints);
+        PlayerPrefs.SetString(savingKey, savingData);
     }
 }
