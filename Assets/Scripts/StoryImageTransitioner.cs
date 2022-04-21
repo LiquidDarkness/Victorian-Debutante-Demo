9cd835707d6b6a903275470a5dfcfab7327a55b7
LiquidDarkness <jumackholio@gmail.com>
2022-04-21 21:09:46 +0200

    - rysowana linia pogrubiona z jednego piksela do lineThickness - linia grubnie w zależności od położenia na osi x (xOffset) - xOffset się animuje

--- Assets/Scripts/StoryImageTransitioner.cs
+++ Assets/Scripts/StoryImageTransitioner.cs
@@ -8,7 +8,13 @@ using UnityEngine.UI;
 public class StoryImageTransitioner : MonoBehaviour
 {
     public Image storyImage;
-    public int xOffset;
+    [Range(0, 10)]
+    public int maxThickness;
+    private int lineThickness;
+    public AnimationCurve xAnimation;
+    public AnimationCurve thicknessAnimation;
+    public float duration;
+    private int xOffset;
     private int pixelAmount;
     private Texture2D texture2D;
     private Color[] clearPixels;
@@ -21,21 +27,37 @@ public class StoryImageTransitioner : MonoBehaviour
         clearPixels = Enumerable.Repeat<Color>(Color.clear, pixelAmount).ToArray();
         Sprite sprite = Sprite.Create(texture2D, new Rect(0, 0, texture2D.width, texture2D.height), Vector2.one / 2.0f);
         storyImage.sprite = sprite;
+        StartCoroutine(LineAnimationRoutine());
     }
 
     private void PaintTexture()
     {
         texture2D.SetPixels(clearPixels);
-        for (int line = 0; line < texture2D.height; line++)
+        for (int i = 0; i < lineThickness; i++)
         {
-            texture2D.SetPixel(xOffset, line, Color.red);
+            for (int line = 0; line < texture2D.height; line++)
+            {
+                texture2D.SetPixel(xOffset + i, line, Color.red);
+            }
         }
         texture2D.Apply();
     }
 
     // Update is called once per frame
-    void Update()
+    IEnumerator LineAnimationRoutine()
     {
-        PaintTexture();
+        float animationTime = 0;
+        while (animationTime < duration)
+        {
+            float xOffsetPercent = xAnimation.Evaluate(animationTime / duration);
+            xOffset = (int)(texture2D.width * xOffsetPercent);  
+            float lineThicknessPercent = thicknessAnimation.Evaluate(xOffsetPercent);
+            Debug.Log(lineThicknessPercent);
+            lineThickness = (int)(maxThickness * lineThicknessPercent);
+            PaintTexture();
+            animationTime += Time.deltaTime;
+            yield return null;
+        }
     }
+
 }
