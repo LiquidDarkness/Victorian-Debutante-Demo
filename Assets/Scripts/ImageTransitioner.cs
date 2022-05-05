9b01fc7de713c4f2ebbebea0cf401d3bfdfcc436
LiquidDarkness <jumackholio@gmail.com>
2022-05-05 20:21:11 +0200

    działająca linia rysująca na pierwszym obrazie drugi obraz + antyaliasing z alfą zależną od odlęgłości od środka ww. linii + storyImages zmienione na textury

---
+++ Assets/Scripts/ImageTransitioner.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageTransitioner : MonoBehaviour
+{
+    public Sprite firstSprite, secondSprite;
+    Texture2D texture;
+    public Image image;
+    [Range(0,360)]
+    public float angle;
+    [Range(-1, 1)]
+    public float yoffset;
+    [Range(0, 1)]
+    public float thickness;
+    public AnimationCurve xAnimation;
+    public AnimationCurve thicknessAnimation;
+    public float duration;
+    Color[] pixels;
+    private Color[] previousStoryPixels;
+    private Color[] currentStoryPixels;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        texture = new Texture2D(firstSprite.texture.width, secondSprite.texture.height);
+        image.sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.one / 2f);
+        pixels = new Color[texture.width * texture.height];
+        previousStoryPixels = firstSprite.texture.GetPixels();
+        currentStoryPixels = secondSprite.texture.GetPixels();
+        texture.SetPixels(previousStoryPixels);
+        texture.Apply();
+
+        PaintTexture();
+    }
+
+    private Color EvaluateLineColor(int x, int y)
+    {
+        float idealY = EvaluateLine(x);
+        float diff = Mathf.Abs(y - idealY);
+        return new Color(1, 0, 0, 1f - diff / thickness / texture.width);
+    }
+
+    private float EvaluateLine(int x)
+    {
+        float a = Mathf.Tan(angle * Mathf.Deg2Rad);
+        float idealY =  a * x + yoffset * texture.height;
+        return idealY;
+    }
+
+    private void PaintTexture()
+    {
+        for (int x = 0; x < texture.width; x++)
+        {
+            for (int y = 0; y < texture.height; y++)
+            {
+                var lineColor = EvaluateLineColor(x, y);
+                Color previousStoryPixel = previousStoryPixels[y * texture.width + x];
+                Color currentStoryPixel = currentStoryPixels[y * texture.width + x];
+                pixels[y * texture.width + x] = Color.Lerp(previousStoryPixel, currentStoryPixel, lineColor.a);
+            }
+        }
+        texture.SetPixels(pixels);
+        texture.Apply();
+    }
+
+    [ContextMenu("Test")]
+    private void Test()
+    {
+        StartCoroutine(LineAnimationRoutine());
+    }
+    IEnumerator LineAnimationRoutine()
+    {
+        float animationTime = 0;
+        while (animationTime < duration)
+        {
+            PaintTexture(animationTime);
+            animationTime += Time.deltaTime;
+            yield return null;
+        }
+        PaintTexture(duration);
+
+        void PaintTexture(float animationTime)
+        {
+            yoffset = xAnimation.Evaluate(animationTime / duration);
+            thickness = thicknessAnimation.Evaluate(yoffset);
+            this.PaintTexture();
+        }
+    }
+}
--- Assets/Scripts/StoryImageTransitioner.cs
+++ Assets/Scripts/StoryImageTransitioner.cs
@@ -7,57 +7,108 @@ using UnityEngine.UI;
 
 public class StoryImageTransitioner : MonoBehaviour
 {
+    public Image glossImage;
     public Image storyImage;
-    [Range(0, 10)]
-    public int maxThickness;
-    private int lineThickness;
+    public Sprite previousStoryImage;
+    public Sprite currentStoryImage;
+    [Range(0, 10)] 
+    public float maxGlossThickness;
+    //public float antyaliasingThickness;
+    [Range(0, 360)]
+    public float angle;
     public AnimationCurve xAnimation;
     public AnimationCurve thicknessAnimation;
     public float duration;
-    private int xOffset;
+    private float lineThickness;
+    public int yOffset;
     private int pixelAmount;
-    private Texture2D texture2D;
+    private int storyPixelAmount;
+    private Texture2D glossTexture2D;
+    private Texture2D storyTexture;
     private Color[] clearPixels;
-    
+    private Color[] previousStoryPixels;
+    private Color[] currentStoryPixels;
+
     // Start is called before the first frame update
     void Start()
     {
-        texture2D = new Texture2D(108, 192);
-        pixelAmount = texture2D.height * texture2D.width;
+        glossTexture2D = new Texture2D(102, 124);
+        pixelAmount = glossTexture2D.height * glossTexture2D.width;
         clearPixels = Enumerable.Repeat<Color>(Color.clear, pixelAmount).ToArray();
-        Sprite sprite = Sprite.Create(texture2D, new Rect(0, 0, texture2D.width, texture2D.height), Vector2.one / 2.0f);
-        storyImage.sprite = sprite;
+        Sprite glossSprite = Sprite.Create(glossTexture2D, new Rect(0, 0, glossTexture2D.width, glossTexture2D.height), Vector2.one / 2.0f);
+        glossImage.sprite = glossSprite;
+
+        storyTexture = new Texture2D(previousStoryImage.texture.width, previousStoryImage.texture.height);
+        Sprite storySprite = Sprite.Create(storyTexture, previousStoryImage.rect, previousStoryImage.pivot);
+        storyImage.sprite = storySprite;
+        previousStoryPixels = previousStoryImage.texture.GetPixels();
+        currentStoryPixels = currentStoryImage.texture.GetPixels();
+        storyPixelAmount = previousStoryPixels.Length;
+        storyTexture.SetPixels(previousStoryPixels);
+        storyTexture.Apply();
+
         StartCoroutine(LineAnimationRoutine());
     }
 
     private void PaintTexture()
     {
-        texture2D.SetPixels(clearPixels);
-        for (int i = 0; i < lineThickness; i++)
+        Color[] glossColors = new Color[pixelAmount];
+        Color[] storyColors = new Color[storyPixelAmount];
+
+        for (int x = 0; x < glossTexture2D.width; x++)
         {
-            for (int line = 0; line < texture2D.height; line++)
+            for (int y = 0; y < glossTexture2D.height; y++)
             {
-                texture2D.SetPixel(xOffset + i, line, Color.red);
+                glossColors[y * glossTexture2D.width + x] = EvaluateLine(x, y, glossTexture2D.height);
             }
         }
-        texture2D.Apply();
+
+        glossTexture2D.SetPixels(glossColors);
+        glossTexture2D.Apply();
+
+        return; 
+        for (int x = 0; x < storyTexture.width; x++)
+        {
+            for (int y = 0; y < storyTexture.height; y++)
+            {
+                var lineColor = EvaluateLine(x, y, storyTexture.height);
+                Color previousStoryPixel = previousStoryPixels[y * storyTexture.width + x];
+                Color currentStoryPixel = currentStoryPixels[y * storyTexture.width + x];
+                storyColors[y * storyTexture.width + x] = Color.Lerp(previousStoryPixel, currentStoryPixel, lineColor.a);
+            }
+        }
+        storyTexture.SetPixels(storyColors);
+        storyTexture.Apply();
+    }
+
+    private Color EvaluateLine(int x, int y, int sizeMultiplier)
+    {
+        float idealY = Mathf.Tan(angle * Mathf.Deg2Rad) * x + (yOffset * sizeMultiplier);
+        float differenceFromIdeal = Mathf.Abs(y - idealY);
+        float alphaValue = 1f - differenceFromIdeal / lineThickness / sizeMultiplier;
+       // alphaValue = Mathf.InverseLerp(lineThickness + antyaliasingThickness, 0, differenceFromIdeal);
+        return new Color(1f, 0f, 0f, alphaValue);
     }
 
     // Update is called once per frame
+    [ContextMenu("Test")]
+    private void Test()
+    {
+        StartCoroutine(LineAnimationRoutine());
+    }
     IEnumerator LineAnimationRoutine()
     {
         float animationTime = 0;
         while (animationTime < duration)
         {
             float xOffsetPercent = xAnimation.Evaluate(animationTime / duration);
-            xOffset = (int)(texture2D.width * xOffsetPercent);  
+            yOffset = (int)(glossTexture2D.width * xOffsetPercent);  
             float lineThicknessPercent = thicknessAnimation.Evaluate(xOffsetPercent);
             Debug.Log(lineThicknessPercent);
-            lineThickness = (int)(maxThickness * lineThicknessPercent);
+            lineThickness = (maxGlossThickness * lineThicknessPercent);
             PaintTexture();
             animationTime += Time.deltaTime;
             yield return null;
         }
     }
-
 }
