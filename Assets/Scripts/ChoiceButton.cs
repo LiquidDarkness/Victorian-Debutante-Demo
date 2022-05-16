e315d58cc7a87a19e019e2966390314214881afc
LiquidDarkness <jumackholio@gmail.com>
2022-05-16 18:50:53 +0200

    comment cleanup

--- Assets/Scripts/ChoiceButton.cs
+++ Assets/Scripts/ChoiceButton.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -14,6 +15,7 @@ public class ChoiceButton : MonoBehaviour
     public Text text;
     public GameObject rootGameObject;
 
+    [UsedImplicitly()]
     public void HandleChoiceMade()
     {
         decisionAnimation.ChooseDecision(index);
@@ -24,10 +26,4 @@ public class ChoiceButton : MonoBehaviour
     {
         choiceText.text = textToSet;
     }
-
-    // TODO: LOW PRIORITY Button should not know about decisionAnimation or Afterchoice.
-    // To, co siê dzieje na scenie potrzebuje swojej klasy, która bêdzie tym wszystkim zarz¹dzaæ (GameplayDisplayManager, AnimationManager, ???)
-    // Dziêki temu, bêdzie mo¿na wo³aæ rownie¿ SetupForDisplay i RestoreDecisions z poziomu kodu.
-    // TODO: disabln¹æ buttonek do przechodzenia do nastêpnej story do momentu podjêcia decyzji (do klasy wy¿ej)
-    // TODO: FadeOut obrazka Story do jakiegoœ fajnego fioletowego czy cuœ.
 }
